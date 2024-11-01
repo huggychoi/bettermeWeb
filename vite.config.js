a569e9ef1d4fb4477049c4a9260d6292baa2dcb1
HookChoi <hook@arpa.co.kr>
2024-11-01 14:06:07 +0900

    Add deploy scripts

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import path from 'path'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/medical-spa-menu/',
+  base: '/bettermeWeb/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
