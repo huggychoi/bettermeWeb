7b71d47a14cd3df3a3d8fb9469d2f8c19bda5a2a
terrypark2003 <34152639+terrypark2003@users.noreply.github.com>
2024-11-08 04:26:57 +0000

    whitening comment

--- src/pages/WhiteningPage.jsx
+++ src/pages/WhiteningPage.jsx
@@ -28,10 +28,10 @@ const whiteningData = {
     },
     {
       title: "피코슈어 토닝",
-      description: "빠른 조사속도, 더 얇은 진피층 거미까지!",
+      description: "빠른 조사속도, 더 얇은 진피층 기미까지!",
       items: [
         {
-          name: "피코슈어 토닝 1회",
+          name: "피코슈어 토닝",
           originalPrice: "137,000",
           salePrices: "69,000"
         },
@@ -41,11 +41,23 @@ const whiteningData = {
           originalPrice: "177,000",
           salePrices: "89,000"
         }
+        {
+          name: "피코슈어 토닝 5회 + 모델링팩 5회 + 줌 1회 패키지 ",
+          description: ,
+          originalPrice: "797,000",
+          salePrices: "399,000"
+        }
+        {
+          name: "피코슈어 토닝 + 엑셀V 제네시스 + 선택관리 ",
+          description: ,
+          originalPrice: "277,000",
+          salePrices: "139,000"
+        }
       ]
     },
     {
       title: "엑셀V 레이저",
-      description: "색소 잡착, 혈관 병변, 모공, 노화 등의 피부 품질 개선",
+      description: "색소 침착, 혈관 병변, 모공, 노화 등의 피부 문제 개선",
       items: [
         {
           name: "엑셀V 제네시스",
@@ -61,6 +73,36 @@ const whiteningData = {
           name: "엑셀V 홍조 얼굴전체",
           originalPrice: "177,000",
           salePrices: "89,000"
+        }
+        {
+          name: "엑셀V 홍조 얼굴전체 + LDM 홍조",
+          originalPrice: "237,000",
+          salePrices: "119,000"
+        }
+        {
+          name: "엑셀V 혈관(부분)",
+          originalPrice: "109,000",
+          salePrices: "55,000"
+        }
+        {
+          name: "엑셀V 혈관(전체)",
+          originalPrice: "157,000",
+          salePrices: "79,000"
+        }
+        {
+          name: "엑셀V 레드스카",
+          originalPrice: "137,000",
+          salePrices: "69,000"
+        }
+        {
+          name: "엑셀V 홍조 + 제네시스",
+          originalPrice: "297,000",
+          salePrices: "149,000"
+        }
+        {
+          name: "엑셀V 홍조 + 혈관",
+          originalPrice: "297,000",
+          salePrices: "149,000"
         }
       ]
     },
@@ -87,7 +129,7 @@ const whiteningData = {
     },
     {
       title: "홍조 케어 프로그램",
-      description: "홍조 제거에 필요한 최적의 치료",
+      description: "홍조 케어에 필요한 최적의 치료",
       items: [
         {
           name: "베리미 홍조 베이직 프로그램(3주)",
@@ -95,19 +137,52 @@ const whiteningData = {
           originalPrice: "797,000",
           salePrices: "399,000"
         }
-      ]
+        items: [
+          {
+            name: "베리미 홍조 프리미엄 프로그램(5주)",
+            description: "[1주]엑셀V홍조 + 라라필 관리 + LED + 모델링팩\n[2주]엑셀V 제네시스 + 엑셀V홍조 + 진정관리 + LED + 모델링팩\n[3주]헐리우드 제네시스 + 엑셀V홍조 + LDM 홍조 + LED + 모델링팩\n [4주] 엑셀V 제네시스 + 엑셀V 홍조+ 진정관리+ 시트팩 \n [5주] 물톡스 + 엑셀V 홍조+ SONO관리+ LED + 모델링팩 ",
+            originalPrice: "1,257,000",
+            salePrices: "629,000"
+          }
+         ]
     },
     {
       title: "피코슈어토닝 미백 프로그램",
-      description: "미백레이저로 색소개선하고 깨끗한 피부 만들기",
+      description: "미백 레이저로 색소 개선하고 깨끗한 피부 만들기",
+      items: [
+        {
+          name: "베리미 피코슈어토닝 미백 관리 (5주)",
+          description: "[1주] 피코슈어토닝 + 라라필관리 + LED + 모델링팩\n[2주] 피코슈어토닝 + SONO 수분관리 + 시트팩\n [3주] 피코슈어토닝 + 피코포커스 + 크라이오 + 모델링팩 \n [4주] 피코슈어토닝 + 비타C 이온토 미백관리 + 시트팩 \n [5주] 피코슈어토닝 + LDM 재생관리 + LED + 모델링팩",
+          originalPrice: "997,000",
+          salePrices: "499,000"
+        }
+      ]
+    } 
+    {
+      title: "프리미엄 미백 프로그램",
+      description: "미백 레이저로 색소 개선하고 깨끗한 피부 만들기",
       items: [
         {
           name: "베리미 프리미엄 미백 프로그램 (10주)",
-          description: "[1주] 헐리우드 듀얼토닝 + 홍옻수 + 진정관리 + 모델링팩\n[2주] 피코슈어토닝 + 프리미엄 미백관리 + 시트팩\n...[10주] 피코슈어토닝 + LDM재생관리 + LED + 모델링팩",
+          description: "[1주] 헐리우드 듀얼토닝 + 물톡스 + 진정관리 + 모델링팩\n[2주] 피코슈어토닝 + 프리미엄 미백관리 + 시트팩\n...[10주] 피코슈어토닝 + LDM재생관리 + LED + 모델링팩",
           originalPrice: "1,997,000",
           salePrices: "999,000"
         }
       ]
+     },
+     {
+      title: "플레티넘 화이트닝 프로그램",
+      description: "미백 레이저로 색소 개선하고 깨끗한 피부 만들기",
+      items: [
+        {
+          name: "플래티넘 화이트닝 프로그램 (10주)",
+          description: "[1주] 피코슈어토닝 + 플래티넘 플라필 + 진정관리 + 모델링팩\n[2주] 피코슈어토닝 + 엑셀V 제네시스 + 이온토 관리 + LED + 모델링팩\n...[10주] 피코슈어토닝 + 엑셀V 제네시스 + LED + 모델링팩",
+          originalPrice: "2,397,000",
+          salePrices: "1,199,000"
+        }
+      ]
+     }
+    ]
     }
   ]
 };
