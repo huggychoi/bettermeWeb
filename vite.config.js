6ca32f6f06e0257f39ca1313c6a630fb861da57b
HookChoi <hook@arpa.co.kr>
2024-11-13 07:19:21 +0900

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/bettermeWeb',
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+  base: '/bettermeWeb/', // GitHub repository 이름으로 수정
 })
