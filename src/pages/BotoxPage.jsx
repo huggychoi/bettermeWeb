eda95aaa04fed1d846d983aa970a3bc0f3330cb5
terrypark2003 <34152639+terrypark2003@users.noreply.github.com>
2024-11-14 06:31:16 +0000

    updated botox comment

--- src/pages/BotoxPage.jsx
+++ src/pages/BotoxPage.jsx
@@ -55,14 +55,19 @@ const botoxData = {
       ]
     },
     {
-      title: "더모톡신",
+      title: "더모톡신(스킨보톡스)",
       description: "잔주름 개선, 탱글탱글한 피부탄력",
       items: [
         {
-          name: "더모톡신(스킨보톡스) 1부위 \n [국산]",
+          name: "더모톡신 1부위 \n [국산]",
           description: "부위: 이마, 볼, 목 중 선택",
           originalPrice: "117,000",
           salePrices: "59,000"
+        },
+        {
+          name: "더모톡신 1부위 \n [나보타]",
+          originalPrice: "147,000",
+          salePrices: "74,000"
         },
         {
           name: "수입 더모톡신 1부위 \n [제오민]",
@@ -78,6 +83,11 @@ const botoxData = {
           name: "더모톡신 얼굴전체 \n [나보타]",
           originalPrice: "197,000",
           salePrices: "99,000"
+        },
+        {
+          name: "수입 더모톡신 얼굴전체 \n [제오민]",
+          originalPrice: "297,000",
+          salePrices: "149,000"
         }
       ]
     },
@@ -86,59 +96,76 @@ const botoxData = {
       description: "특수부위 보톡스",
       items: [
         {
-          name: "광대 보톡스 [국산]",
+          name: "광대보톡스 [국산]",
           originalPrice: "59,000",
           salePrices: "30,000"
         },
         {
-          name: "귀밑 침샘 보톡스 50u [국산]",
-          originalPrice: "77,000",
-          salePrices: "39,000"
+          name: "광대보톡스 [나보타]",
+          originalPrice: "119,000",
+          salePrices: "60,000"
         },
         {
-          name: "귀밑 침샘 보톡스 50u [나보타]",
-          originalPrice: "137,000",
-          salePrices: "69,000"
+          name: "귀밑 침샘 보톡스 50u \n [국산]",
+          originalPrice: "97,000",
+          salePrices: "49,000"
         },
         {
-          name: "측두근 보톡스 50u [국산]",
-          originalPrice: "137,000",
-          salePrices: "69,000"
-        },
-        {
-          name: "측두근 보톡스 50u [나보타]",
+          name: "귀밑 침샘 보톡스 50u \n [나보타]",
           originalPrice: "157,000",
           salePrices: "79,000"
+        },
+        {
+          name: "측두근 보톡스 50u \n [국산]",
+          originalPrice: "137,000",
+          salePrices: "69,000"
+        },
+        {
+          name: "측두근 보톡스 50u \n [나보타]",
+          originalPrice: "177,000",
+          salePrices: "89,000"
         }
       ]
     },
     {
       title: "다한증 보톡스",
-      description: "원래 어디서나 보송보송 해지자",
+      description: "언제 어디서나 뽀송뽀송",
       items: [
         {
-          name: "다한증 보톡스 50u [국산]",
+          name: "다한증 보톡스 50u \n [국산]",
           originalPrice: "137,000",
           salePrices: "69,000"
         },
         {
-          name: "다한증 보톡스 100u [국산]",
+          name: "다한증 보톡스 100u \n [국산]",
           originalPrice: "197,000",
           salePrices: "99,000"
         },
         {
-          name: "수입 다한증보톡스 100u [제오민]",
-          originalPrice: "437,000",
-          salePrices: "219,000"
+          name: "수입 다한증보톡스 50u \n [제오민]",
+          originalPrice: "297,000",
+          salePrices: "149,000"
+        },
+        {
+          name: "수입 다한증보톡스 100u \n [제오민]",
+          originalPrice: "477,000",
+          salePrices: "239,000"
         }
       ]
     },
     {
-      title: "브이올렛 주사",
+      title: "골드 XV주사/브이올렛",
+      description: "무스테로이드 프리미엄 지방세포 파괴 주사", 
       items: [
         {
+          name: "골드 XV주사 20cc 얼굴 1부위",
+          description: "허리는 X로, 얼굴은 V로 잘록하고 갸름하게",
+          originalPrice: "117,000",
+          salePrices: "59,000"
+        },
+        {
           name: "브이올렛 주사 2cc 1회",
-          description: "이중턱 파괴!! 지방세포 자체를 파괴, 콜라겐 합성 도움으로 턱선 유지까지! \n지방 세포의 수가 줄어 반영구적인 효과 *이중턱 복원 가능",
+          description: "이중턱 파괴!! 지방세포 자체를 파괴, 콜라겐 합성 도움으로 턱선 유지까지! \n지방 세포의 수가 줄어 반영구적인 효과 ",
           originalPrice: "497,000",
           salePrices: "249,000"
         }
