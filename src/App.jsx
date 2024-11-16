254eba478ed8432c323c7b80113bc620ec66ca19
HookChoi <hook@arpa.co.kr>
2024-11-16 19:13:51 +0900

    Please no more overwrite

--- src/App.jsx
+++ src/App.jsx
@@ -187,7 +187,7 @@ function App() {
             </div>
           </div>
           
-          <div className="max-w-4xl mx-auto p-4">
+          <div className="max-w-4xl mx-auto p-4 min-h-0">
             {renderActivePage()}
 
             {/* 부가세 안내 */}
